d95de04be201addcfbe232b597ae948b130c1ce7
Dhruvit Kachhiya <>
2022-04-26 13:09:47 +0530

    Project Created

---
+++ NSC_iOS/ViewControllers/SplashVC.swift
@@ -0,0 +1,22 @@
+//
+//  SplashVC.swift
+//  NSC_iOS
+//
+//  Created by Dhruvit on 26/04/22.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+    
+    
+    // MARK: - VIEW LIFE CYCLE
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+}
