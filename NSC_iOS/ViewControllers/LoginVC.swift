31b18c4724e31687d087c4d5f71b8a3b60b1f1ce
sapna kardas <>
2022-10-06 12:53:21 +0530

    again remove space

--- NSC_iOS/ViewControllers/LoginVC.swift
+++ NSC_iOS/ViewControllers/LoginVC.swift
@@ -30,6 +30,7 @@ class LoginVC: BaseViewController {
     //UIStackView
     @IBOutlet weak var stackView: UIStackView!
     
+    
     // MARK: - VARIABLES
     var loginCheckVM : LoginCheckViewModel?
     var isFromOTP = false
